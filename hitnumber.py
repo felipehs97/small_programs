c396bf8fcde99f02513d8e2db56736e0f0f01f2c
Felipe Araujo <felipemraraujo@gmail.com>
2023-06-19 08:45:38 -0300

    commit4

--- hitnumber.py
+++ hitnumber.py
@@ -1,19 +1,17 @@
 import random
 
-print ("Chute um valor de 1 a 10 para tentar acertar o valor aleatório")
-valor_aleatorio = random.randint (1,10)
-Errou = False
+print("Chute um valor de 1 a 10 para tentar acertar o valor aleatório")
+random_value = random.randint(1, 10)
+wrong_guess = False
 while True:
-    chute_usuario = int (input("Digite o seu chute: "))
-    if valor_aleatorio > chute_usuario:
-        print ("O valor chutado foi menor que o valor aleatório")
-    elif valor_aleatorio < chute_usuario:
-        print ("O valor chutado foi maior que o valor aleatório")
-    elif valor_aleatorio == chute_usuario:
-        print ("Você Acertou")
-        Errou = False
+    user_guess = int(input("Digite seu chute: "))
+    if random_value > user_guess:
+        print("O valor chutado foi menor que o valor aleatório")
+    elif random_value < user_guess:
+        print("O valor chutado foi maior que o valor aleatório")
+    elif random_value == user_guess:
+        print("Você Acertou")
+        wrong_guess = False
         break
     else:
         None
-    
-
